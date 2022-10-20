2813f2d54fdfc684714cce849f0fd45b8c4692da
Lucas Dzin Pedroso <lucasdzin@gmail.com>
2022-10-20 09:31:03 -0300

    change favicon

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -3,10 +3,15 @@ import type { AppProps } from 'next/app'
 import { ThemeProvider } from '@mui/material'
 import Header from '../src/components/Header'
 import Theme from '../src/themes/theme'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={Theme}>
+      <Head>
+        <title>My Teacher</title>
+        <link rel="icon" type="image/x-icon" href="/images/favicon.png" />
+      </Head>
       <Header />
       <Component {...pageProps} />
     </ThemeProvider>
